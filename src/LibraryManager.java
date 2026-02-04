ea520b08f648a26cd850e2726a41edb51045463d
Nguyen Trung Kien <nguyentrungkien040921@gmail.com>
2026-02-04 14:19:24 +0700

    delete readme của a thai

--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -4,8 +4,7 @@ public class LibraryManager {
 private List<Book> books = new ArrayList<>();
 public void addBook(Book book) {
 books.add(book);
-System.out.println("Da them sach thanh
-cong!");
+System.out.println("Da them sach thanh cong!");
 }
 public void displayBooks() {
 for (Book b : books) {
